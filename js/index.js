817be7c10da6d9ff5fccd4eaf925ec9909286eda
Vadim <vadzzimluch@gmail.com>
2022-09-01 14:35:28 -0400

    Fetch API and comments from Kira Miller

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-
+/* Footer name and year */ 
 let today = new Date();
 let thisYear = today.getFullYear();
 let footer = document.querySelector('footer');
@@ -7,8 +7,8 @@ let copyright = document.createElement('p');
 copyright.innerHTML = "&nbsp;&nbsp;&copy; Vadim Dmitrochenko, " + thisYear;;
 footer.appendChild(copyright);
 
+/* Skills */
 let skills = ['HTML', 'CSS','JavaScript','Github','Google Services','VS-Code','Debugging'];
-let skills = ['HTML', 'CSS','JavaScript','Google Services','Github','VS-Code','Debugging'];
 let skillsSection = document.getElementById('skills');
 let skillsList = skillsSection.querySelector('ul');
 
@@ -17,36 +17,34 @@ for (let i = 0; i < skills.length; i++) {
   skill.innerHTML = `${skills[i]}`;
   skillsList.appendChild(skill);
 }
-
+ /* List of messages display */
 let messageSection = document.querySelector('#messages');
 let messageList = messageSection.querySelector('ul');
 let messageForm = document.querySelector('[name = "leave_message"]');
 /* New Message Submission */
-messageForm.addEventListener('submit', (event) => {
-  event.preventDefault();
+  messageForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+let name = event.target.name;
+let email = event.target.email;
+let message = event.target.message;
+let newMessage = document.createElement('li');
 
-  let name = event.target.name;
-  let email = event.target.email;
-  let message = event.target.message;
-  let newMessage = document.createElement('li');
-
-  newMessage.innerHTML = "<a href='mailto:" + email.value + "'>" + name.value + "</a><span>  wrote: " + message.value + "</span>   ";
-
+newMessage.innerHTML = "<a href='mailto:" + email.value + "'>" + name.value + "</a><span>  wrote: " + message.value + "</span>   ";
+/* Remove button */
   let removeButton = document.createElement('button');
-
-  removeButton.innerText = "Remove";
-  removeButton.innerText = "remove";
-  removeButton.setAttribute("type", "button");
-  removeButton.addEventListener('click', (event) => {
+    removeButton.innerText = "Remove";
+    /* removeButton.innerText = "remove"; */
+    removeButton.setAttribute("type", "button");
+    removeButton.addEventListener('click', (event) => {
   let entry = removeButton.parentNode;
-  entry.remove();
-  messagesSectionShowHide();
+    entry.remove();
+    messagesSectionShowHide();
 });
-  newMessage.appendChild(removeButton);
-  messageList.appendChild(newMessage);
-  console.log (name.value, email.value, message.value);
-  messagesSectionShowHide();
-  messageForm.reset();
+   
+    newMessage.appendChild(removeButton);
+    messageList.appendChild(newMessage);
+    console.log (name.value, email.value, message.value);
+    messageForm.reset();
 });
   
 function messagesSectionShowHide(){
@@ -55,5 +53,48 @@ function messagesSectionShowHide(){
   } else {
       messageSection.style.display = "none";
   }
-}
-
+};
+//Fetch GitHub Repositories//
+/*let githubRequest = new XMLHttpRequest();
+  githubRequest.open("GET" , "https://api.github.com/users/VadimDmitr/repos");
+  githubRequest.send ();
+//Handle Response from Server//
+githubRequest.onreadystatechange = function () {
+    if (githubRequest.readyState === 4) {
+        let repositories = JSON.parse(githubRequest.responseText);
+        console.log(repositories);
+//Display Repositories//
+        let projectSection = document.getElementById("projects");
+        let projectList = projectSection.querySelector ("ul");
+    
+        for (let i=0; i<repositories.length; i++) {
+            let project = document.createElement('li');
+            let repoLink = document.createElement('a');
+              repoLink.href = repositories[i].html_url;
+              repoLink.innerHTML = repositories[i].name;
+              project.appendChild(repoLink);
+              projectList.appendChild(project);
+          
+        }
+    }
+};*/
+// Fetch API //
+fetch("https://api.github.com/users/VadimDmitr/repos")
+.then(response => response.json())
+.then(repositories => {
+//Display Repositories//
+  console.log(repositories);
+    let projectSection = document.getElementById("projects");
+    let projectList = projectSection.querySelector ("ul");
+    
+      for (let i=0; i<repositories.length; i++) {
+        let project = document.createElement('li');
+        let repoLink = document.createElement('a');
+          repoLink.href = repositories[i].html_url;
+          repoLink.innerHTML = repositories[i].name;
+          project.appendChild(repoLink);
+          projectList.appendChild(project);
+          
+        }
+    })
+    .catch(error => alert("Something went wrong!", error));
